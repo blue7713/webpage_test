86cfe6d9d793a610adf38f9f847781af82e71a14
Ryu hyeon seok <gustjr1058@naver.com>
2024-02-13 18:23:53 +0900

    front_blog

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const backToTop = document.getElementById('backtotop');
+
+function checkScroll() {
+    /*
+      웹페이지가 수직으로 얼마나 스크롤되었는지를 확인하는 값(픽셀 단위로 반환)
+      https://developer.mozilla.org/ko/docs/Web/API/Window/pageYOffset
+    */
+    if (window.scrollY !== 0) {
+        backToTop.classList.add('show');
+    } else {
+        backToTop.classList.remove('show');
+    }
+};
+
+function moveBackToTop() {
+    if (window.scrollY > 0) {
+         /*
+      웹페이지가 수직으로 얼마나 스크롤되었는지를 확인하는 값(픽셀 단위로 반환)
+      https://developer.mozilla.org/ko/docs/Web/API/Window/pageYOffset
+    */
+       window.scrollTo({top: 0, behavior: "smooth"})
+    }
+};
+
+window.addEventListener('scroll', checkScroll);
+backToTop.addEventListener('click', moveBackToTop);
