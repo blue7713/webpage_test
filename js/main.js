76f7f03e8eaf1f060bec59157c40cf8afe97f7d3
Ryu Hyeon Seok <gustjr1058@naver.com>
2024-02-13 21:44:30 +0900

    making

--- js/main.js
+++ js/main.js
@@ -1,26 +1,49 @@
 const backToTop = document.getElementById('backtotop');
 
-function checkScroll() {
+const checkScroll = () => {
     /*
       웹페이지가 수직으로 얼마나 스크롤되었는지를 확인하는 값(픽셀 단위로 반환)
       https://developer.mozilla.org/ko/docs/Web/API/Window/pageYOffset
     */
-    if (window.scrollY !== 0) {
+    if (scrollY !== 0) {
         backToTop.classList.add('show');
     } else {
         backToTop.classList.remove('show');
     }
-};
+}
 
-function moveBackToTop() {
-    if (window.scrollY > 0) {
-         /*
-      웹페이지가 수직으로 얼마나 스크롤되었는지를 확인하는 값(픽셀 단위로 반환)
-      https://developer.mozilla.org/ko/docs/Web/API/Window/pageYOffset
-    */
-       window.scrollTo({top: 0, behavior: "smooth"})
+const moveBackToTop = () => {
+    if (scrollY > 0) {
+        /*
+        smooth 하게 스크롤하기
+        https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTo
+        */
+        window.scrollTo({ top: 0, behavior: "smooth" })
     }
-};
+}
 
 window.addEventListener('scroll', checkScroll);
 backToTop.addEventListener('click', moveBackToTop);
+
+
+const transformPrev = () => {
+
+}
+
+const slidePrevList = document.getElementsByClassName("slide-prev");
+
+for (let i = 0; i < slidePrevList.length; 1++) {
+    // ul 태그 선택
+    let classList = slidePrevList[i].parentElement.parentElement.nextElementSibling;
+    let liList = classList.getElementsByTagName("li");
+
+    // 카드가 태그 너비보다 넘치면, 왼쪽(PREV) 버튼을 활성화하고, 오른쪽(NEXT)는 현재 맨 첫카드 위치이므로 비활성화
+    if (classList.clientWidth < (liList.length * 260)) {
+        slidePrevList[i].classList.add("slide-prev-hover");
+        slidePrevList[i].addEventListener("click", transformPrev);
+    } else {
+        const arrowContainer = slidePrevList[i].parentElement;
+        arrowContainer.removeChild(slidePrevList[i].nextElementSibling);
+        arrowContainer.removeChild(slidePrevList[i]);
+    }
+}
